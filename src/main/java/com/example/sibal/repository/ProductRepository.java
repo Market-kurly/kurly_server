75566ed3f02924b9b10fe261dda588241df3dc2d
taehyun9244 <85472257+taehyun9244@users.noreply.github.com>
2022-03-21 23:58:15 +0900

    Market Kurly

--- src/main/java/com/example/sibal/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.sibal.repository;
-
-import com.example.sibal.domain.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
